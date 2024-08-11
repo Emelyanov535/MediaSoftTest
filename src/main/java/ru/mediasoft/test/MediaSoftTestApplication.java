f7140af853f12fc3fd5b50e827b3fe51a6cd1bf8
Emalyanov535 <nouctanch777093@gmail.com>
2024-08-11 17:31:50 +0400

    initial project

---
+++ src/main/java/ru/mediasoft/test/MediaSoftTestApplication.java
@@ -0,0 +1,13 @@
+package ru.mediasoft.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaSoftTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaSoftTestApplication.class, args);
+    }
+
+}
